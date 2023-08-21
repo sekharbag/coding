6fbd8ea447c3716dc89b0e71a67b1c699d272721
sekharbag <sekharb2000@gmail.com>
2023-08-21 14:17:08 +0530

    next

--- c++/mergesort/mergesort.cpp
+++ c++/mergesort/mergesort.cpp
@@ -89,12 +89,11 @@ int main()
     vector<int>v;
     v.push_back(5);
     v.push_back(1);
-    v.push_back(8);
-    v.push_back(2);
     v.push_back(3);
-   v.push_back(9);
+    v.push_back(0);
+    v.push_back(4);
+    v.push_back(2);
     v.push_back(6);
-     v.push_back(4);
     mergesort(v);
-    cout<<c;
+    cout<<c+1;
 }
